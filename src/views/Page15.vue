47fe866e27d9329b0088f8adfc27441b48413e8c
lzn <lzn@lzndeMacBook-Pro.local>
2022-03-18 20:48:03 +0900

    15fixed

--- src/views/Page15.vue
+++ src/views/Page15.vue
@@ -7,7 +7,9 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import GUI from 'lil-gui'
 import { onMounted } from 'vue'
-import { BufferGeometry, Material, Points } from 'three'
+import { BufferGeometry, Points, ShaderMaterial } from 'three'
+import vertex from '../shaders/page15/vertex.glsl'
+import fragment from '../shaders/page15/fragment.glsl'
 const init = () => {
   const sizes = {
     width: window.innerWidth,
@@ -24,8 +26,8 @@ const init = () => {
   const scene = new THREE.Scene()
   // galaxy
   const parameters = {
-    count: 100000,
-    size: 0.01,
+    count: 200000,
+    size: 0.005,
     radius: 5,
     branches: 3,
     spin: 1,
@@ -35,7 +37,7 @@ const init = () => {
     outsideColor: '#1b3984'
   }
   let geometry: null | BufferGeometry = null
-  let material: null | Material = null
+  let material: null | ShaderMaterial = null
   let points: Points | null = null
   const generateGalaxy = () => {
     if (points !== null) {
@@ -47,50 +49,61 @@ const init = () => {
     }
     geometry = new THREE.BufferGeometry()
     const positions = new Float32Array(parameters.count * 3)
+    const randomness = new Float32Array(parameters.count * 3)
     const colors = new Float32Array(parameters.count * 3)
+    const scales = new Float32Array(parameters.count * 1)
 
-    const colorInside = new THREE.Color(parameters.insideColor)
-    const colorOutside = new THREE.Color(parameters.outsideColor)
-
-    colorInside.lerp(colorOutside, 0.5)
+    const insideColor = new THREE.Color(parameters.insideColor)
+    const outsideColor = new THREE.Color(parameters.outsideColor)
 
     for (let i = 0; i < parameters.count; i++) {
       const i3 = i * 3
 
       // positions
       const radius = Math.random() * parameters.radius
-      const spinAngle = radius * parameters.spin
-      const branchAngle = i % parameters.branches / parameters.branches * Math.PI * 2
+      const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
 
-      const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? -1 : 1)
-      const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? -1 : 1)
-      const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? -1 : 1)
-      positions[i3 + 0] = Math.cos(branchAngle + spinAngle) * radius + randomX
-      positions[i3 + 1] = randomY
-      positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ
+      const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? -1 : 1) * parameters.randomness * radius
+      const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * parameters.randomness * radius
+      const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1) * parameters.randomness * radius
+
+      positions[i3 + 0] = Math.cos(branchAngle) * radius
+      positions[i3 + 1] = 0
+      positions[i3 + 2] = Math.sin(branchAngle) * radius
+
+      randomness[i3 + 0] = randomX
+      randomness[i3 + 1] = randomY
+      randomness[i3 + 2] = randomZ
 
       // colors
-      const mixedColor = colorInside.clone()
-      mixedColor.lerp(colorOutside, radius / parameters.radius)
+      const mixedColor = insideColor.clone()
+      mixedColor.lerp(outsideColor, radius / parameters.radius)
       colors[i3 + 0] = mixedColor.r
       colors[i3 + 1] = mixedColor.g
       colors[i3 + 2] = mixedColor.b
+
+      // scales
+      scales[i] = Math.random()
     }
     geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+    geometry.setAttribute('aRandomness', new THREE.BufferAttribute(randomness, 3))
     geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
-    material = new THREE.PointsMaterial({
-      size: parameters.size,
-      sizeAttenuation: true,
+    geometry.setAttribute('aScale', new THREE.BufferAttribute(scales, 1))
+    material = new THREE.ShaderMaterial({
       depthWrite: false,
       blending: THREE.AdditiveBlending,
-      vertexColors: true
+      vertexColors: true,
+      uniforms: {
+        uTime: { value: 0 },
+        uSize: { value: 30 * renderer.getPixelRatio() }
+      },
+      vertexShader: vertex,
+      fragmentShader: fragment
     })
     points = new THREE.Points(geometry, material)
     scene.add(points)
   }
-  generateGalaxy()
-  gui.add(parameters, 'count').min(10000).max(900000).step(100).onFinishChange(generateGalaxy)
-  gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
+  gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
   gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
   gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
   gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy)
@@ -99,16 +112,20 @@ const init = () => {
   gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
   gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
   const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
-  camera.position.z = 5
+  camera.position.x = 3
+  camera.position.y = 3
+  camera.position.z = 3
+  scene.add(camera)
   const renderer = new THREE.WebGLRenderer({ antialias: true })
   renderer.setSize(sizes.width, sizes.height)
   renderer.setPixelRatio(window.devicePixelRatio)
   document.body.appendChild(renderer.domElement)
   const controls = new OrbitControls(camera, renderer.domElement)
+  generateGalaxy()
   const clock = new THREE.Clock()
   const tick = () => {
     const elapsedTime = clock.getElapsedTime()
-
+    material!.uniforms.uTime.value = elapsedTime
     controls.update()
     renderer.render(scene, camera)
     requestAnimationFrame(tick)
